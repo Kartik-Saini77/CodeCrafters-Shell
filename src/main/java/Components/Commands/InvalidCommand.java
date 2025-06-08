113109bc6b7327012b875587af34d378fc8092b4
Kartik_Saini77 <kartiksaini2904@gmail.com>
2025-06-09 03:29:35 +0530

    type command and running programs

--- src/main/java/Components/Commands/InvalidCommand.java
+++ src/main/java/Components/Commands/InvalidCommand.java
@@ -4,6 +4,6 @@ public class InvalidCommand implements Command{
 
     @Override
     public String execute(String[] args) {
-        return String.join(" ", args) + ": command not found";
+        return String.join(" ", args) + ": command not found\r\n";
     }
 }
