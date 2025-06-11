d4dbd4998534bd16abfb2de3e305ab5260f87c55
Kartik_Saini77 <kartiksaini2904@gmail.com>
2025-06-11 16:42:41 +0530

    Added support for single quotes

--- src/main/java/Components/Services/CommandParser.java
+++ src/main/java/Components/Services/CommandParser.java
@@ -2,11 +2,48 @@ package Components.Services;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class CommandParser {
 
     public String[] parse(String command) {
+        command = command.trim();
+        List<String> list = new ArrayList<>();
+        int n = command.length();
+        int last = 0;
+        boolean isInQuotes = false;
 
-        return command.split(" ");
+        for (int i = 0; i < n; i++) {
+            char ch = command.charAt(i);
+            if (!isInQuotes && ch == ' ') {
+                String com = command.substring(last, i);
+                if (!com.isEmpty()) list.add(com);
+                int t = i;
+                while(t<n && command.charAt(t) == ' ') t++;
+                last = t;
+            } else if (ch == '\'') {
+                if (!isInQuotes) {
+                    isInQuotes = true;
+                    last = i + 1;
+                } else {
+                    String com = command.substring(last, i);
+                    if (!com.isEmpty()) list.add(com);
+                    isInQuotes = false;
+                    int t = i;
+                    do t++;
+                    while (t<n && command.charAt(t) == ' ');
+                    last = t;
+                    i = last - 1;
+                }
+            }
+        }
+        if (last != n-1) {
+            String com = command.substring(last, n);
+            if (!com.isEmpty()) list.add(com);
+        }
+
+        return list.toArray(new String[0]);
     }
 }
